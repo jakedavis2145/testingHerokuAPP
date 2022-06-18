b852e124990f6c61b2041df495f1fbe71f128844
Jake Davis <jake.davis2145@gmail.com>
2022-06-17 17:22:36 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app =express();
+const importData = require("./data.json");
+let port = process.env.PORT || 3000;
+
+app.get("/", (req,res) => {
+    res.send("hello world")
+});
+
+app.get("/lists", (req, res) => {
+    res.send(importData);
+});
+
+app.listen(port, () => {
+    console.log(`example app is listening on port http://localhost:${port}`)
+});
+
